e85c837bd46c8da867450c8a4416a60f7a72a3bf
Tim Lewis <timlewis@Tims-MacBook-Pro.local>
2016-10-12 20:51:18 -0500

    basic up and running test

--- tests/steps/everything.py
+++ tests/steps/everything.py
@@ -1,16 +1,23 @@
+import urllib2
+
 @given("The service is running")
 def step_impl(context):
     pass
 
 @when('I request an api endpoint')
 def step_impl(context):
-    pass
+    context.service_url = 'http://localhost:8080/dividends'
+    url = context.service_url
+   
+ 
+    fp = urllib2.urlopen(url)
+    context.status_code = fp.getcode()
 
 
 
 @then('The service should be reachable')
 def step_impl(context):
-    pass
+    assert context.status_code is 200
 
 
 
