dede5c0740338666452df2ba60149c8065f961fb
tlewis11 <timlewis4645@gmail.com>
2017-09-04 00:59:44 -0500

    add summary method

--- income_portfolio.py
+++ income_portfolio.py
@@ -1,9 +1,10 @@
-import urllib2
+import logging
+import json
+import urllib.request
 from bs4 import BeautifulSoup
 
-
-
-portfolio = [('wmt', 4),
+#TODO: read this from a config file
+my_positions = [('wmt', 4),
              ('ko', 25),
              ('cvx', 27),
              ('ge', 25),
@@ -14,26 +15,50 @@ portfolio = [('wmt', 4),
              ('pfe', 2),
 	         ('vlo', 9)]
 
+
+def test_analyze_return_value():
+    portfolio = Portfolio(positions=[('xom', 1)])
+        
+    assert 'yearly_income' in analyze(portfolio.positions).keys()
+
+
+#def analyze(por
+class Portfolio(object):
+    def __init__(self, positions=[]):
+        self.positions = positions 
+
+    def summary(self):
+        print(self.positions)
+        print(analyze(self.positions))
 def get_dividend_amount(symbol):
     '''
     Return dividend amount for given symbol.
     Goes to nasdaq dividned history page, looks up given stock symbol.
     '''
     stock_url = 'http://www.nasdaq.com/symbol/%s/dividend-history' %symbol
-    remote_fp = urllib2.urlopen(stock_url)
+    remote_fp = urllib.request.urlopen(stock_url)
 
     soup = BeautifulSoup(remote_fp.read(), 'html.parser')
 
     amount = soup.find('table', id='quotes_content_left_dividendhistoryGrid').find('tbody').find('tr').find_all('td')[2].find('span').renderContents()
     
-    print '%s: %s' %(symbol, amount)
+    #print '%s: %s' %(symbol, amount)
     remote_fp.close()
     
     return amount
 
-total = 0
-for component in portfolio:
-    
-    total = total + (float(get_dividend_amount(component[0])) * component[1] * 4)
-    
-print 'Total yearly income: %s' %total
+def analyze(portfolio):
+    total = 0
+    for component in portfolio:
+        print(component) 
+        total = total + (float(get_dividend_amount(component[0])) * component[1] * 4)
+        
+    return {'yearly_income': total}
+
+
+def main():
+    portfolio = Portfolio(positions=my_positions)     
+    portfolio.summary()
+if __name__ == '__main__':
+    #test_analyze_return_value()
+    main()
